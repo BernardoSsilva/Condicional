66f47bd65fe735e02048c6b28916221539c1a6d9
Bernardo <126777966+BernardoSsilva@users.noreply.github.com>
2023-03-23 09:05:05 -0300

    Add files via upload

---
+++ Treino/mediaAluno/media.java
@@ -0,0 +1,31 @@
+package mediaAluno;
+
+import java.util.Scanner;
+
+public class media {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Digite o nome do aluno: ");
+        String nome = scan.next();
+        System.out.print("Digite a primeira nota: ");
+        float primNota = scan.nextFloat();
+        System.out.print("Digite a segunda nota: ");
+        float segNota = scan.nextFloat();
+        float media = (primNota + segNota)/2;
+        if(media >= 6){
+            System.out.print("O aluno "+nome+" foi aprovado");
+            System.out.printf(", com media %.1f",media);
+        } else {
+            System.out.print("O aluno "+nome+" foi para recuperação");
+            System.out.printf(", com media %.1f",media);
+            System.out.println(" ");
+             System.out.print("Digite a nota da recuperação: ");
+            float notaRec = scan.nextFloat();
+            if (notaRec >= 6){
+                System.out.printf("O aluno "+nome+" Foi aprovado com nota %.1f",notaRec);
+            } else {
+                System.out.printf("O aluno "+nome+" Foi reprovado com nota %.1f",notaRec);
+            }
+        }
+    }
+}
